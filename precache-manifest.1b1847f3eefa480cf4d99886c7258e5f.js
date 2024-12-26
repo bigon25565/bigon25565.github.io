d3290e8353624c28d1e2dce69126b831030a33b3
Gkamentsev <54892100+bigon25565@users.noreply.github.com>
2024-12-26 13:36:03 +0300

    Add files via upload

---
+++ precache-manifest.1b1847f3eefa480cf4d99886c7258e5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6562346b70a3d8b53663e93b009102dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9ba3a9b1fa135ed75c",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "de9ba3a9b1fa135ed75c",
+    "url": "/static/js/main.003d22f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
